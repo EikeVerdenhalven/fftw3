f0dfc24e51a32e49d82094d0bb762e955fe04ad1
Eike Verdenhalven <eike.v@gmx.de>
2017-01-21 01:55:33 +0100

    merge in travis integration

---
+++ .travis-build.sh
@@ -0,0 +1,5 @@
+mkdir -p ../build-fftw
+cd ../build-fftw
+../cmake-3.7.2/bin/cmake -G Ninja -DFFTW_ENABLE_TESTS=ON -DFFTW_BUILD_THREADS=ON -DFFTW_MAINTENANCE_MODE=ON -DFFTW_SIMD_USE_SSE2=ON -DFFTW_USE_AMALGAMATES=ON -DCMAKE_BUILD_TYPE=Release ../fftw3
+ninja
+ninja fftw_tarball
