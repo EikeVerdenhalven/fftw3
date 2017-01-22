bdc1390c7b8b9328d70158c4a55cfb5a03bc8e70
Eike Verdenhalven <eike.v@gmx.de>
2017-01-22 17:28:53 +0100

    travis: add zip file

--- .travis-build.sh
+++ .travis-build.sh
@@ -3,3 +3,4 @@ cd ../build-fftw
 ../cmake-3.7.2/bin/cmake -G Ninja -DFFTW_ENABLE_TESTS=ON -DFFTW_BUILD_THREADS=ON -DFFTW_MAINTENANCE_MODE=ON -DFFTW_SIMD_USE_SSE2=ON -DFFTW_USE_AMALGAMATES=ON -DCMAKE_BUILD_TYPE=Release ../fftw3
 ninja
 ninja fftw_tarball
+ninja fftw_zip
